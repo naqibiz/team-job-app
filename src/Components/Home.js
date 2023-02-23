4b99b589b12e9535d3744744375aa471b1bb93af
Muhammad Naqi <naqimng786@gmail.com>
2023-02-23 21:24:12 +0500

    fixed changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,22 +8,36 @@ const HomePage = () => {
       <Typography variant="h3" color={theme.palette.info.main}>
         MUI Next Js Project Team Job
       </Typography>
-      <Grid container>
-        <Grid md={4} display="flex" justifyContent="center" alignItems="center">
+      <Grid container spacing={1}>
+        <Grid item md={4}>
           <Box sx={{ backgroundColor: theme.palette.primary.main }}>
             <Typography variant="h5" color={theme.palette.info.main}>
               Column One
             </Typography>
           </Box>
         </Grid>
-        <Grid md={4} display="flex" justifyContent="center" alignItems="center">
+        <Grid item md={4}>
           <Box sx={{ backgroundColor: theme.palette.primary.main }}>
             <Typography variant="h5" color={theme.palette.info.main}>
               Column One
             </Typography>
           </Box>
         </Grid>
-        <Grid md={4} display="flex" justifyContent="center" alignItems="center">
+        <Grid item md={4}>
+          <Box sx={{ backgroundColor: theme.palette.primary.main }}>
+            <Typography variant="h5" color={theme.palette.info.main}>
+              Column One
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item md={4}>
+          <Box sx={{ backgroundColor: theme.palette.primary.main }}>
+            <Typography variant="h5" color={theme.palette.info.main}>
+              Column One
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item md={4}>
           <Box sx={{ backgroundColor: theme.palette.primary.main }}>
             <Typography variant="h5" color={theme.palette.info.main}>
               Column One
