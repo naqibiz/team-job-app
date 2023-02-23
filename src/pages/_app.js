60e9ecc7681c6156010e1965e9f8bd1a9a3f5da6
Muhammad Naqi <naqimng786@gmail.com>
2023-02-23 19:30:57 +0500

    fixed changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 // import "@/styles/globals.css";
+import { theme } from "@/styles/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }) {
   return (
